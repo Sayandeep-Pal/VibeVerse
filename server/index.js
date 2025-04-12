fc2d29f727cf8f3527e5df3891de43f5faca1994
Sayandeep-Pal <145487074+Sayandeep-Pal@users.noreply.github.com>
2025-04-12 13:15:51 +0530

    limit increased

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const app = express();
 const port = 5000;
 
 app.use(cors());
-app.use(express.json({ limit: "50mb" })); // Increase limit for base64 data
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "100mb" })); // Increase limit for base64 data
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
 require("dotenv").config();
 mongoose
   .connect(process.env.MONGODB, {
